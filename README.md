3d20246d3d6f8ca84dc2b770e95ef8fbef02f014
h04x <liquid.doctrine@gmail.com>
2023-05-07 10:44:41 +0300

    tiny changes

--- README.md
+++ README.md
@@ -7,3 +7,26 @@ Partial implementations
  
 
 ## Example
+```Rust
+fn main() {
+    let addr = "1.1.1.1:1111";
+    let my_peer_id = [0; 20];
+    let info_hash = [0; 20];
+
+    let s = TcpStream::connect(addr).unwrap();
+    let t = PeerProto::handshake(s, info_hash, my_peer_id);
+    if t.is_err() {
+        println!(
+            "peer {} connected but handshake failed due to {:?}",
+            addr, t
+        );
+    }
+    let p = Arc::new(t.unwrap());
+
+    let msg = p.recv().unwrap();
+    let bitfield = match msg {
+        Message::Bitfield(bf) => bf,
+        _ => panic!(),
+    };
+}
+```
--- examples/main.rs
+++ examples/main.rs
@@ -1,27 +1,27 @@
 use std::{net::TcpStream, sync::Arc};
 
-use bittorrent_peer_proto::peer_proto::{PeerProto, Message};
+use bittorrent_peer_proto::peer_proto::{Message, PeerProto};
 
 //use bittorrent_peer_proto::{Message, PeerProto};
 
 fn main() {
-let addr = "1.1.1.1:1111";
-let my_peer_id = [0; 20];
-let info_hash = [0; 20];
+    let addr = "1.1.1.1:1111";
+    let my_peer_id = [0; 20];
+    let info_hash = [0; 20];
 
-let s = TcpStream::connect(addr).unwrap();
-let t = PeerProto::handshake(s, info_hash, my_peer_id);
-if t.is_err() {
-    println!(
-        "peer {} connected but handshake failed due to {:?}",
-        addr, t
-    );
-}
-let p = Arc::new(t.unwrap());
+    let s = TcpStream::connect(addr).unwrap();
+    let t = PeerProto::handshake(s, info_hash, my_peer_id);
+    if t.is_err() {
+        println!(
+            "peer {} connected but handshake failed due to {:?}",
+            addr, t
+        );
+    }
+    let p = Arc::new(t.unwrap());
 
-let msg = p.recv().unwrap();
-let bitfield = match msg {
-    Message::Bitfield(bf) => bf,
-    _ => panic!()
-};
+    let msg = p.recv().unwrap();
+    let bitfield = match msg {
+        Message::Bitfield(bf) => bf,
+        _ => panic!(),
+    };
 }
